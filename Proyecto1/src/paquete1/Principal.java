ea23659f868e06f3c92aa39a4c302e5d39c4e580
mgarcias <mgarcias@lliurexadmin-e70sff>
2023-03-28 20:30:34 +0200

    Otro cambio

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -3,6 +3,7 @@ package paquete1;
 public class Principal {
 		public static void main(String[] args) {
 			System.out.println("Control de versiones con Git");
+			System.out.println("Otro mensaje");
 		}
 
 }
