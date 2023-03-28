6dfb4de9d661df5fc3672e692b4991499224c498
mgarcias <mgarcias@lliurexadmin-e70sff>
2023-03-28 20:40:36 +0200

    Cambio definitivo

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 		public static void main(String[] args) {
 			System.out.println("Control de versiones con Git");
 			System.out.println("Otro mensaje");
+			System.out.println("Último mensaje mostrado");
 		}
 
 }
